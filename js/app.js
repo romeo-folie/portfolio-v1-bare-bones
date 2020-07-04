af53e5b12279c4c86aae6738bf11d58870a55c49
Romeo Folie <romeofolie1@gmail.com>
2020-07-04 13:03:56 +0000

    added sidenav

--- js/app.js
+++ js/app.js
@@ -9,3 +9,20 @@ window.onscroll = function() {
   }
   prevScrollpos = currentScrollPos;
 }
+
+function showSideNav() {
+  let sideNavToggle = document.getElementById('side-nav');
+  if(sideNavToggle.className === 'side-menu'){
+    sideNavToggle.className += ' show';
+  } else {
+    sideNavToggle.className = 'side-menu'
+  }
+}
+
+
+function hideSideNav(){
+  let sideNavToggle = document.getElementById('side-nav');
+  if(sideNavToggle.className === 'side-menu show'){
+    sideNavToggle.className = 'side-menu'
+  }
+}
